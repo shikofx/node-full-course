612ff8e89e71ecf55c7c1e09d134ec81a6013fd4
Dzmitry Parkheichuk <Dzmitry_Parkheichuk@epam.com>
2019-09-24 11:21:56 +0300

    L46. Use express server to send html pages, js, css

---
+++ web-server/pages/js/app.js
@@ -0,0 +1,1 @@
+console.log('JS script for pages is loaded');
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,41 +1,29 @@
 const express = require('express');
+const path = require('path');
 const weather = require('../../weather-app/utils/weather')
+
 const app = express();
 
-console.log(__filename);
-console.log(__dirname)
-app.get('', (req, res) => {
-    // res.send('../pages/index.html')
-    res.send('<h1>MAIN page</h1>'); //Send html code
-});
+const pagesPath = path.join(__dirname, '../pages');
+
+app.use(express.static(pagesPath));
 
 app.get('/weather', (req, res) => {
     weather.findByPlace('Moscow', (w) => {
-        res.send(JSON.stringify(w));
+        res.send(JSON.stringify(w)); 
     });
 });
 
 app.get('/help', (req, res) => {
-    // res.send('../pages/help.html');
-
+    res.sendFile(pagesPath + '/help.html');
 });
 
 app.get('/about', (req, res) => {
-    // res.send('../pages/about.html');
-    res.send([{
-        city: "Minsk", 
-        name: "Andrew", 
-        age: 25
-    }, {
-        city: "Brest", 
-        name: "Vasya", 
-        age: 34
-    }])
+    
+    res.sendFile(pagesPath + '/about.html');
 }); 
 
 app.listen(3000, () => {
-    // console.log('Server is up on port 3000');
+    console.log('Server is up on port 3000');
+    
 });
-// app.com
-// app.com/help
-// app.com/about
