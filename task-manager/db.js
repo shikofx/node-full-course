f1044dc0b0f9843fbcf64e6c372729ebffa647ea
Dzmitry Parkheichuk <Dzmitry_Parkheichuk@epam.com>
2019-10-08 15:42:09 +0300

    S10. MongoDB. Add first data to collection

--- task-manager/db.js
+++ task-manager/db.js
@@ -1,15 +1,21 @@
 const colorLog = require('chalk').default;
 
-const db = require('mongodb');
-const dbClient = db.MongoClient;
+const mongodb = require('mongodb');
+const mongoClient = mongodb.MongoClient;
 
-const dbUrl = 'mongodb://127.0.0.1:27017';
+const mongoUrl = 'mongodb://127.0.0.1:27017';
 const dbName = 'task-manager';
 
-dbClient.connect(dbUrl, { useNewUrlParser: true }, (error, client) => {
+mongoClient.connect(mongoUrl, { useNewUrlParser: true }, (error, client) => {
     if(error){
         return console.log(colorLog.red.inverse('Unable to connect to the database'));
     }
+
+    const db = client.db(dbName);
+    db.collection('users').insertOne({
+        name: 'Dzmitry',
+        age: 35
+    });
 
     console.log(colorLog.green.inverse('Connection succed!!!'));
 });
