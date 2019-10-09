d959e15d53321ab3c7345d332aecfdf9a568d4ef
Dzmitry Parkheichuk <Dzmitry_Parkheichuk@epam.com>
2019-10-09 16:14:39 +0300

    S10. MongoDB. insertMany with result.ops

--- task-manager/db.js
+++ task-manager/db.js
@@ -6,16 +6,39 @@ const mongoClient = mongodb.MongoClient;
 const mongoUrl = 'mongodb://127.0.0.1:27017';
 const dbName = 'task-manager';
 
-mongoClient.connect(mongoUrl, { useNewUrlParser: true }, (error, client) => {
+mongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if(error){
         return console.log(colorLog.red.inverse('Unable to connect to the database'));
     }
 
     const db = client.db(dbName);
-    db.collection('users').insertOne({
-        name: 'Dzmitry',
-        age: 35
+    // db.collection('users').insertOne({
+    //     name: 'Yarik',
+    //     age: 35
+    // }, (error, result) => {
+    //     if(error){
+    //         return console.log(colorLog.bgRed(error.code + ': ' + error.message));
+    //     }
+    //     console.log(result.ops);
+    // });
+
+    // console.log(colorLog.green.inverse('Connection succed!!!'));
+
+    db.collection('users').insertMany([{
+        name: 'Tanya',
+        age: 37
+    }, {
+        name: 'Yarik',
+        age: 14
+    }, {
+        name: 'Arsi',
+        age: 3
+    }], (error, result) => {
+        if(error){
+            return console.log(colorLog.bgRed(error.code + ': ' + error.message));
+        }
+        console.log(result.ops);
     });
 
-    console.log(colorLog.green.inverse('Connection succed!!!'));
+    
 });
