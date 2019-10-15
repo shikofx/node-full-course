e8ed352c4163851afb7f8afcc9470ebcf878b39a
Dzmitry Parkheichuk <Dzmitry_Parkheichuk@epam.com>
2019-10-15 12:01:54 +0300

    TM. CRUD

--- task-manager/db.js
+++ task-manager/db.js
@@ -1,5 +1,5 @@
 const colorLog = require('chalk').default;
-
+const assert = require('assert');
 // const mongodb = require('mongodb');
 // const mongoClient = mongodb.MongoClient;
 // const ObjectID = mongodb.ObjectID;
@@ -10,29 +10,78 @@ const mongoUrl = 'mongodb://127.0.0.1:27017';
 const dbName = 'task-manager';
 
 const id = new ObjectID();
-console.log(id);
-console.log(id.id.length)
-console.log(id.id);
-console.log(id.toHexString().length)
-console.log(id.getTimestamp())
-
 
 MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if(error){
-        return console.log(colorLog.red.inverse('Unable to connect to the database'));
+        return console.log(colorLog.bgRed(`Error ${error.code}: ${error.message}`));
     }
-
+    
+    console.log('Connected correctly to server');
+    
     const db = client.db(dbName);
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Yarik',
-        age: 35
-    }, (error, result) => {
-        if(error){
-            return console.log(colorLog.bgRed(error.code + ': ' + error.message));
-        }
-        console.log(result.ops);
-    });
+    
+    // db.collection('users').findOne({ name: 'Yarik', age: 35 }, (error, user) => {
+    //     if(error){
+    //         return console.log(colorLog.bgRed(`Error ${error.code}: ${error.message}`));
+    //     }
+        
+    //     console.log(user);
+    // });
+
+    // db.collection('users').findOne({ _id: new ObjectID("5d9dd89a53c92181ac509e2f") }, (error, user) => {
+    //     if(error){
+    //         return console.log(colorLog.bgRed(`Error ${error.code}: ${error.message}`));
+    //     }
+        
+    //     console.log(user);
+    // });
+    
+    // db.collection('users').find({ age: 35 }).count((error, count) => {
+    //     if(error){
+    //         return console.log(colorLog.bgRed(`Error ${error.code}: ${error.message}`));
+    //     }
+
+    //     console.log('It is found ' + count + ' items');
+    // });
+
+    // db.collection('users').find({ age: 35 }).toArray((error, users) => {
+    //     if(error){
+    //         return console.log(colorLog.bgRed(`Error ${error.code}: ${error.message}`));
+    //     }
+    //     console.log(users);
+    // });
+
+    // db.collection('tasks').findOne( { _id: new ObjectID('5d9ddd5faca4275e48a13202') } , (error, task) => {
+    //     if(error){
+    //         return console.log(colorLog.bgRed(`Error ${error.code}: ${error.message}`));
+    //     }
+
+    //     console.log(task);
+    // });
+
+
+
+    // db.collection('tasks').find( { completed: true } ).toArray((error, tasks) => {
+    //     if(error){
+    //         return console.log(colorLog.bgRed(`Error ${error.code}: ${error.message}`));
+    //     }
+        
+    //     console.log(tasks);
+    // })
+    // db.collection('users').find({ name: 'Yarik' }).toArray((error, users) => {
+    //     console.log(users);
+    // });
+    
+        
+    // db.collection('users').s
+    // db.collection('users').insertOne({
+    //     _id: id,
+    //     name: 'Yarik',
+    //     age: 35
+    // }, (error, result) => {
+        
+    //     console.log(result.ops);
+    // });
 
     
     // db.collection('users').insertMany([{
@@ -53,10 +102,10 @@ MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true 
 
     // db.collection('tasks').insertMany([{
     //     description: 'Insert docements to database',
-    //     completed: false
+    //     completed: true
     // }, {
     //     description: 'Print log with result of the inserting',
-    //     completed: false
+    //     completed: true
     // }], (error, result) => {
     //     if(error) {
     //         console.log(colorLog.bgRed(error.code + ": " + error.message));
